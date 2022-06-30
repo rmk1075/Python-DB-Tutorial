a22c4097a58405c7475104d0093af8a1fdd36b39
jamie <rmk1075@gmail.com>
2022-06-30 17:37:50 +0000

    fix: session 관련 추가

--- sqlalchemy-test/models/base.py
+++ sqlalchemy-test/models/base.py
@@ -1,17 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-engine = None
-Session = None
+engine = create_engine('sqlite:///test.db')
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 Base = declarative_base()
 
 def init_db():
-    engine = create_engine('sqlite:///test.db')
-    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+    # engine = create_engine('sqlite:///test.db')
+    # Session = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
     Base.metadata.create_all(engine)
 
 
 class ModelBase(Base):
     __abstract__ = True
+    session = Session()
     
