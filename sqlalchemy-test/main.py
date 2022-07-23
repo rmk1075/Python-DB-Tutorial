6b88187d826bf1c4a8f838cdf1089b0aed2158d0
jamie <rmk1075@gmail.com>
2022-07-23 11:59:53 +0000

    test: create team and user update test code 추가

--- sqlalchemy-test/main.py
+++ sqlalchemy-test/main.py
@@ -5,18 +5,28 @@ from service import Service
 def run():
     init_db()
     service = Service()
-    print(f"service.get_users()::{service.get_users()}")
-    print(f"service.add_user(user_id='test_id', name='test_name')::{service.add_user(user_id='test_id', name='test_name')}")
-    print(f"service.get_users()::{service.get_users()}")
-    # print(f"service.add_user(user_id='test_id', name='test_name')::{service.add_user(user_id='test_id', name='test_name')}")
-    # print(f"service.get_user()::{service.get_users()}")
     
-    for i in range(10):
-        user = service.add_user(user_id=str(i), name=f"user{i}")
-        print(f"added user: {user}")
-
-    print(f"service.get_users()::{service.get_users()}")
-
-
+    team1 = service.create_team(name="team1")
+    print(f"new team. team1={team1}")
+    
+    user1 = service.create_user(user_id="user1", name="one", team_id=team1.id)
+    print(f"new user. user1={user1}")
+    user2 = service.create_user(user_id="user2", name="two", team_id=team1.id)
+    print(f"new user. user2={user2}")
+    
+    print("--------------------------------------------------")
+    
+    team2 = service.create_team(name="team2")
+    print(f"new team. team2={team2}")
+    user = service.update_user(user_id="user2", team_id=team2.id)
+    print(f"update user. user={user}")
+    
+    print("--------------------------------------------------")
+    
+    print("print out users of each team")
+    print(team1.users)
+    print(team2.users)
+    
+    
 if __name__ == '__main__':
     run()
